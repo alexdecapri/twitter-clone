5a3c20537dd0e9370ab1295325dc31a8b46d2005
alexdecapri <alexdecapri@gmail.com>
2016-02-12 16:22:38 -0700

    counter and change to red are working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+	$(".tweet-compose").on("click", function() {
+		$(this).css("height", "5em");
+		$("#tweet-controls").css("display", "inline-block");
+		$(".button").css("display", "inline-block");
+	});
+
+
+	$(".tweet-compose").blur(function() {
+		$(".tweet-compose").css("height", "2.5em");
+		$("#tweet-controls").css("display", "none");
+		$(".button").css("display", "none");
+	});
+
+
+	$(".tweet-compose").keydown(function() {
+		var text = $("#char-count").text();
+		var num = parseInt(text);
+		if (event.keyCode === 8) {
+			if (num !== 140) {
+				++num;
+			}
+		}
+		else {
+			--num;
+		}
+		if (num <= 10) {
+			$("#char-count").css("color", "red");
+		}
+		$("#char-count").text(num);
+	});
+
+
 
 
 });
